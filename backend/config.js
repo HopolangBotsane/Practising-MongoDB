fe53faeeca36e99f66df4cd1677884a333fb1505
Thato Hopolang Botsane <thbjr@outlook.com>
2025-04-10 09:07:38 +0200

    I don't know

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://root:root@mern-book-store.lv6ts57.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Mern-Book-Store';
-
-// Please create a free database for yourself
-// This database will be deleted after the tutorial
